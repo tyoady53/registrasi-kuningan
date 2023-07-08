5b4e0b0a285be832834fe0f71c55de03e7b78c72
tyoady53 <adyprasetyo.0103@gmail.com>
2023-07-09 04:33:32 +0700

    first upload

---
+++ app/Http/Controllers/ChoiceTestController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChoiceTest;
+use Illuminate\Http\Request;
+
+class ChoiceTestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ChoiceTest $choiceTest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ChoiceTest $choiceTest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ChoiceTest $choiceTest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ChoiceTest $choiceTest)
+    {
+        //
+    }
+}
